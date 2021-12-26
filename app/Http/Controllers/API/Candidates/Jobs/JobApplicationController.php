c519045ac0c787c1e34c20898b6fb86c12fdc126
prolaxu <adhikarilaxu0001@gmail.com>
2021-12-26 18:51:59 +0545

    Feature - API Candidate Jobs Application

---
+++ app/Http/Controllers/API/Candidates/Jobs/JobApplicationController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API\Candidates\Jobs;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Traits\Api\ApiMethods;
+use DB;
+use App\Models\Job;
+use App\Models\Employe;
+use Illuminate\Support\Facades\Auth;
+
+class JobApplicationController extends Controller
+{
+    use ApiMethods;
+
+    public function apply(Request $request){
+        $request->validate([
+            'job_id'=>'required|unique:job_applications'
+        ]);
+        $employ=Employe::where([
+            'user_id'=>Auth::user()->id
+        ])->first();
+        $job= DB::table('job_applications')
+        ->insert(
+            [
+                'employ_id'=>$employ->id,
+                'job_id'=>$request->job_id,
+                'status'=>'pending',
+                "created_at" =>  \Carbon\Carbon::now(), # new \Datetime()
+                "updated_at" => \Carbon\Carbon::now(),  # new \Datetime()
+        ]);
+        return $this->sendResponse(Job::find($request->job_id),"Job Application Submitted.");
+    }
+    public function list(Request $request){
+        // dd($request);
+        $jobs = [];
+        $job_applications= DB::table('job_applications')->where('status',$request->status)->get();
+        foreach($job_applications as $index=>$application){
+            $jobs[$index]=Job::find($application->job_id);
+        }
+        return $this->sendResponse($jobs,"Jobs Applications List.");
+    }
+}
