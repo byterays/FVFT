d195ebe5c66c29d8c7bf5914c9227ff317d2d57f
prolaxu <adhikarilaxu0001@gmail.com>
2022-01-10 13:29:19 +0545

    Enhc - Hero section

--- resources/views/themes/fvft/site/components/hero.blade.php
+++ resources/views/themes/fvft/site/components/hero.blade.php
@@ -12,25 +12,24 @@
                         <div class="search-background bg-transparent">
                             <div class="form row no-gutters ">
                                 <div class="form-group  col-xl-4 col-lg-3 col-md-12 mb-0 bg-white ">
-                                    <input type="text" class="form-control input-lg br-tr-md-0 br-br-md-0" id="text4" placeholder="Search Jobs">
+                                    <input type="text" class="form-control input-lg br-tr-md-0 br-br-md-0" id="text4" placeholder="Search Jobs" name="search">
                                 </div>
                                 <div class="form-group  col-xl-3 col-lg-3 col-md-12 mb-0 bg-white">
-                                    <input type="text" class="form-control input-lg br-md-0" id="text5" placeholder="Select Location">
-                                    <span><img src="{{asset('themes/fvft/')}}/assets/images/svg/gps.svg" class="location-gps" alt="img"></span>
+                                    {{-- <input type="text" class="form-control input-lg br-md-0" id="text5" placeholder="Select Location"> --}}
+                                    <select class="form-control select2-show-search  border-bottom-0" data-placeholder="Select Country" id="select-country" name="country">
+                                        <option>All Countries</option>
+                                        @foreach ($countries as $item)
+                                            <option value="{{$item->id}}">{{$item->name}}</option>
+                                        @endforeach
+                                    </select>
                                 </div>
                                 <div class="form-group col-xl-3 col-lg-3 col-md-12 select2-lg  mb-0 bg-white">
-                                    <select class="form-control select2-show-search  border-bottom-0" data-placeholder="Select Category">
+                                    <select class="form-control select2-show-search  border-bottom-0" data-placeholder="Select Category" name="category">
                                         <optgroup label="Categories">
                                             <option>All Categories</option>
-                                            <option value="1">Accountant</option>
-                                            <option value="2">IT Software</option>
-                                            <option value="3">Banking</option>
-                                            <option value="4">Finaces</option>
-                                            <option value="5">Cook/Chef</option>
-                                            <option value="6">Driveing</option>
-                                            <option value="7">HR Recruiter</option>
-                                            <option value="8">IT Hardware</option>
-                                            <option value="9">Sales</option>
+                                            @foreach ($job_categories as $item)
+                                                <option value="{{$item->id}}">{{$item->functional_area}}</option>
+                                            @endforeach
                                         </optgroup>
                                     </select>
                                 </div>
@@ -46,198 +45,24 @@
         <div class="header-slider-img">
             <div class="container">
                 <div id="small-categories" class="owl-carousel owl-carousel-icons7">
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-primary-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/operator.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3 mt-0">Call Centers</h5>
-                                        <small class="badge badge-pill badge-primary mr-2">7,485 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-secondary-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/cashier.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Cashier</h5>
-                                        <small class="badge badge-pill badge-secondary mr-2">3,451 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-success-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/operator.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">BPO</h5>
-                                        <small class="badge badge-pill badge-success mr-2">4,758 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-danger-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/truck.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Delivery Jobs</h5>
-                                        <small class="badge badge-pill badge-danger mr-2">6,457 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-warning-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/hand.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Life Insurance</h5>
-                                        <small class="badge badge-pill badge-warning mr-2">2,875 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-info-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/presentation.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Teacher/lecturer</h5>
-                                        <small class="badge badge-pill badge-info mr-2">2,154 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-primary-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/nurse.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Nurse</h5>
-                                        <small class="badge badge-pill badge-primary mr-2">1,785 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-secondary-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/web.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">IT Software</h5>
-                                        <small class="badge badge-pill badge-secondary mr-2">2,456 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-success-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/charts.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Sales</h5>
-                                        <small class="badge badge-pill badge-success mr-2">1,456 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-info-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/chauffer.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Driver</h5>
-                                        <small class="badge badge-pill badge-info mr-2">896 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-warning-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/makeup.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Beautician</h5>
-                                        <small class="badge badge-pill badge-warning mr-2">1,478 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <div class="card mb-0">
-                            <div class="card-body p-3">
-                                <div class="cat-item d-flex">
-                                    <a href="jobs-list.html"></a>
-                                    <div class="cat-img mr-4 bg-danger-transparent p-3 brround">
-                                        <img src="{{asset('themes/fvft/')}}/assets/images/products/categories/cooking.png" alt="img">
-                                    </div>
-                                    <div class="cat-desc text-left">
-                                        <h5 class="mb-3">Cooking</h5>
-                                        <small class="badge badge-pill badge-danger mr-2">2,424 Jobs</small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @foreach ($job_categories as $item)
+                                                <div class="item">
+                                                    <div class="card mb-0">
+                                                        <div class="card-body p-3">
+                                                            <div class="cat-item d-flex">
+                                                                <a href="/jobs/{{$item->id}}"></a>
+                                                                <div class="cat-img mr-4 bg-primary-transparent p-3 brround">
+                                                                    <img src="https://avatars.dicebear.com/api/initials/{{$item->functional_area}}.svg" alt="img">
+                                                                </div>
+                                                                <div class="cat-desc text-left">
+                                                                    <h5 class="mb-3 mt-0">{{$item->functional_area}}</h5>
+                                                                    <small class="badge badge-pill badge-primary mr-2">{{\DB::table('jobs')->where(['job_categories_id'=>$item->id])->count()}}Jobs</small>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            @endforeach
                 </div>
             </div>
         </div>
