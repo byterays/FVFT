3c6d1edbd13b9abbd87bfc9c5f5b9e4ef61d993d
prolaxu <adhikarilaxu0001@gmail.com>
2022-01-10 16:47:18 +0545

    Enhc - Candidate Dashboard  and Candidate Auth

---
+++ resources/views/themes/fvft/candidates/components/sidebar.blade.php
@@ -0,0 +1,36 @@
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">My Dashboard</h3>
+        </div>
+        <div class="card-body text-center item-user border-bottom">
+            <div class="profile-pic">
+                <div class="profile-pic-img">
+                    <span class="bg-success dots" data-toggle="tooltip" data-placement="top" title="" data-original-title="online"></span>
+                    <img src="../assets/images/users/male/25.jpg" class="brround" alt="user">
+                </div>
+                <a href="userprofile.html" class="text-dark"><h4 class="mt-3 mb-0 font-weight-semibold">Robert McLean</h4></a>
+            </div>
+        </div>
+        <div class="item1-links  mb-0">
+            <a href="/candidate/profile" class="d-flex border-bottom">
+                <span class="icon1 mr-2"><i class="typcn typcn-edit fs-20"></i></span> Profile
+            </a>
+            <a href="/candidate/myjobs" class="active  d-flex  border-bottom">
+                <span class="icon1 mr-2"><i class="typcn typcn-briefcase fs-20"></i></span> My Jobs
+            </a>
+            <a href="/pages/tips" class="d-flex border-bottom">
+                <span class="icon1 mr-2"><i class="typcn typcn-flag-outline fs-20"></i></span> Safety Tips
+            </a>
+            <a href="/candidate/settings" class="d-flex border-bottom">
+                <span class="icon1 mr-2"><i class="typcn typcn-cog-outline fs-20"></i></span> Settings
+            </a>
+            <a  class="d-flex">
+                <form action="/candidate/logout" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-secondary mb-1 mt-1">
+                        <span class="icon1 mr-2" style="background: #ffffff00;color: #f3f6ff;"><i class="typcn typcn-power-outline fs-20"></i></span> Logout
+                    </button>
+                </form>
+            </a>
+        </div>
+    </div>
