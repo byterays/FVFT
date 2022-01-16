b7df7fbc8fca81dcf62ddb109fd782a6d99a7ab3
prolaxu <adhikarilaxu0001@gmail.com>
2022-01-16 17:19:36 +0545

    Enhc - Site News

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Traits\Site\ThemeMethods;
+
 class HomeController extends Controller
 {
     use ThemeMethods;
-    public function home(){
-   
-        return $this->site_view('site.home');
+    public function home()
+    {
+        $news = \DB::table('news')->where('is_active', 1)->orderBy('id', 'desc')->limit(10)->get();
+        return $this->site_view('site.home', ['news' => $news]);
     }
 }
--- resources/views/themes/fvft/site/components/news.blade.php
+++ resources/views/themes/fvft/site/components/news.blade.php
@@ -3,183 +3,35 @@
 			<div class="container">
 				<div class="section-title center-block text-center">
 					<h1>News</h1>
-					<p>Mauris ut cursus nunc. Morbi eleifend, ligula at consectetur vehicula</p>
+				
 				</div>
 				<div id="defaultCarousel" class="owl-carousel Card-owlcarousel owl-carousel-icons">
+					@foreach ($news as $item)	
 					<div class="item">
 						<div class="card mb-0">
 							<div class="item7-card-img">
-								<a href="#"></a>
-								<img src="{{asset('themes/fvft/')}}/assets/images/products/products/ed1.jpg" alt="img" class="cover-image">
+								<a href="news/{{$item->slug}}"></a>
+								<img src="{{asset('/')}}{{$item->feature_img}}" alt="img" class="cover-image">
 							</div>
 							<div class="card-body p-4">
 								<div class="item7-card-desc d-flex mb-2">
-									<a href="#"><i class="fa fa-calendar-o text-muted mr-2"></i>Dec-03-2018</a>
-									<div class="ml-auto">
-										<a href="#"><i class="fa fa-comment-o text-muted mr-2"></i>4 Comments</a>
-									</div>
+									<a href="#"><i class="fa fa-calendar-o text-muted mr-2"></i>{{ \Carbon\Carbon::parse($item->updated_at)->diffForhumans() }}</a>
+									
 								</div>
-								<a href="blog-details.html" class="text-dark"><h4 class="font-weight-semibold">Excepteur occaecat cupidatat</h4></a>
-								<p>Ut enim ad minima veniam, quis exercitationem, enim ad minima veniam, quis nostrum exercitationem </p>
+								<a href="news/{{$item->slug}}" class="text-dark"><h4 class="font-weight-semibold">{{ $item->title }}</h4></a>
+								<p>{{  Str::limit($item->short_description,50) }} </p>
 								<div class="d-flex align-items-center pt-2 mt-auto">
-									<img src="{{asset('themes/fvft/')}}/assets/images/users/male/5.jpg" class="avatar brround avatar-md mr-3" alt="avatar-img">
+									<img src="{{asset('/')}}uploads/site/fvft_favicon.png" class="avatar brround avatar-md mr-3" alt="avatar-img">
 									<div>
-										<a href="profile.html" class="text-default">Joanne Nash</a>
-										<small class="d-block text-muted">1 day ago</small>
+										<a href="profile.html" class="text-default">FreeVisaFreeTicket</a>
+										<small class="d-block text-muted">{{ \Carbon\Carbon::parse($item->created_at)->diffForHumans() }}</small>
 									</div>
-									<div class="ml-auto text-muted">
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fa fa-thumbs-o-up"></i></a>
-									</div>
+									
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="item">
-						<div class="card mb-0">
-							<div class="item7-card-img">
-								<a href="#"></a>
-								<img src="{{asset('themes/fvft/')}}/assets/images/products/products/j2.jpg" alt="img" class="cover-image">
-							</div>
-							<div class="card-body p-4">
-								<div class="item7-card-desc d-flex mb-2">
-									<a href="#"><i class="fa fa-calendar-o text-muted mr-2"></i>Nov-28-2018</a>
-									<div class="ml-auto">
-										<a href="#"><i class="fa fa-comment-o text-muted mr-2"></i>2 Comments</a>
-									</div>
-								</div>
-								<a href="blog-details.html" class="text-dark"><h4 class="font-weight-semibold">Sed ut perspiciatis unde iste</h4></a>
-								<p>Ut enim ad minima veniam, quis exercitationem, enim ad minima veniam, quis nostrum exercitationem </p>
-								<div class="d-flex align-items-center pt-2 mt-auto">
-									<img src="{{asset('themes/fvft/')}}/assets/images/users/male/7.jpg" class="avatar brround avatar-md mr-3" alt="avatar-img">
-									<div>
-										<a href="profile.html" class="text-default">Tanner Mallari</a>
-										<small class="d-block text-muted">2 days ago</small>
-									</div>
-									<div class="ml-auto text-muted">
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fa fa-thumbs-o-up"></i></a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<div class="card mb-0">
-							<div class="item7-card-img">
-								<a href="#"></a>
-								<img src="{{asset('themes/fvft/')}}/assets/images/products/products/ed2.jpg" alt="img" class="cover-image">
-							</div>
-							<div class="card-body p-4">
-								<div class="item7-card-desc d-flex mb-2">
-									<a href="#"><i class="fa fa-calendar-o text-muted mr-2"></i>Nov-19-2018</a>
-									<div class="ml-auto">
-										<a href="#"><i class="fa fa-comment-o text-muted mr-2"></i>8 Comments</a>
-									</div>
-								</div>
-								<a href="blog-details.html" class="text-dark"><h4 class="font-weight-semibold">At vero eos et accusamus et iusto</h4></a>
-								<p>Ut enim ad minima veniam, quis exercitationem, enim ad minima veniam, quis nostrum exercitationem </p>
-								<div class="d-flex align-items-center pt-2 mt-auto">
-									<img src="{{asset('themes/fvft/')}}/assets/images/users/female/15.jpg" class="avatar brround avatar-md mr-3" alt="avatar-img">
-									<div>
-										<a href="profile.html" class="text-default">Aracely Bashore</a>
-										<small class="d-block text-muted">5 days ago</small>
-									</div>
-									<div class="ml-auto text-muted">
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fa fa-thumbs-o-up"></i></a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<div class="card mb-0">
-							<div class="item7-card-img">
-								<a href="#"></a>
-								<img src="{{asset('themes/fvft/')}}/assets/images/products/products/ed5.jpg" alt="img" class="cover-image">
-							</div>
-							<div class="card-body p-4">
-								<div class="item7-card-desc d-flex mb-2">
-									<a href="#"><i class="fa fa-calendar-o text-muted mr-2"></i>Dec-03-2018</a>
-									<div class="ml-auto">
-										<a href="#"><i class="fa fa-comment-o text-muted mr-2"></i>4 Comments</a>
-									</div>
-								</div>
-								<a href="blog-details.html" class="text-dark"><h4 class="font-weight-semibold">Excepteur occaecat cupidatat</h4></a>
-								<p>Ut enim ad minima veniam, quis exercitationem, enim ad minima veniam, quis nostrum exercitationem </p>
-								<div class="d-flex align-items-center pt-2 mt-auto">
-									<img src="{{asset('themes/fvft/')}}/assets/images/users/male/15.jpg" class="avatar brround avatar-md mr-3" alt="avatar-img">
-									<div>
-										<a href="profile.html" class="text-default">Royal Hamblin</a>
-										<small class="d-block text-muted">10 days ago</small>
-									</div>
-									<div class="ml-auto text-muted">
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fa fa-thumbs-o-up"></i></a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<div class="card mb-0">
-							<div class="item7-card-img">
-								<a href="#"></a>
-								<img src="{{asset('themes/fvft/')}}/assets/images/products/products/j3.jpg" alt="img" class="cover-image">
-							</div>
-							<div class="card-body p-4">
-								<div class="item7-card-desc d-flex mb-2">
-									<a href="#"><i class="fa fa-calendar-o text-muted mr-2"></i>Nov-28-2018</a>
-									<div class="ml-auto">
-										<a href="#"><i class="fa fa-comment-o text-muted mr-2"></i>2 Comments</a>
-									</div>
-								</div>
-								<a href="blog-details.html" class="text-dark"><h4 class="font-weight-semibold">Sed ut perspiciatis unde omnis iste</h4></a>
-								<p>Ut enim ad minima veniam, quis exercitationem, enim ad minima veniam, quis nostrum exercitationem </p>
-								<div class="d-flex align-items-center pt-2 mt-auto">
-									<img src="{{asset('themes/fvft/')}}/assets/images/users/female/5.jpg" class="avatar brround avatar-md mr-3" alt="avatar-img">
-									<div>
-										<a href="profile.html" class="text-default">Rosita Chatmon</a>
-										<small class="d-block text-muted">10 days ago</small>
-									</div>
-									<div class="ml-auto text-muted">
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fa fa-thumbs-o-up"></i></a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="item">
-						<div class="card mb-0">
-							<div class="item7-card-img">
-								<a href="#"></a>
-								<img src="{{asset('themes/fvft/')}}/assets/images/products/products/ed7.jpg" alt="img" class="cover-image">
-							</div>
-							<div class="card-body p-4">
-								<div class="item7-card-desc d-flex mb-2">
-									<a href="#"><i class="fa fa-calendar-o text-muted mr-2"></i>Nov-19-2018</a>
-									<div class="ml-auto">
-										<a href="#"><i class="fa fa-comment-o text-muted mr-2"></i>8 Comments</a>
-									</div>
-								</div>
-								<a href="blog-details.html" class="text-dark"><h4 class="font-weight-semibold">At vero eos et accusamus et iusto</h4></a>
-								<p>Ut enim ad minima veniam, quis exercitationem, enim ad minima veniam, quis nostrum exercitationem </p>
-								<div class="d-flex align-items-center pt-2 mt-auto">
-									<img src="{{asset('themes/fvft/')}}/assets/images/users/male/6.jpg" class="avatar brround avatar-md mr-3" alt="avatar-img">
-									<div>
-										<a href="profile.html" class="text-default">Loyd Nolf</a>
-										<small class="d-block text-muted">15 days ago</small>
-									</div>
-									<div class="ml-auto text-muted">
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fe fe-heart mr-1"></i></a>
-										<a href="javascript:void(0)" class="icon d-none d-md-inline-block ml-3"><i class="fa fa-thumbs-o-up"></i></a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+					@endforeach
 				</div>
 			</div>
 		</section>
