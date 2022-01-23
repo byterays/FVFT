77e9bc77d93ed2d3ee89f4a07d47da228e7c2c5a
prolaxu <adhikarilaxu0001@gmail.com>
2022-01-23 14:39:56 +0545

    Enhc - API Candidate CV Upload.

--- routes/v1/api/candidate.php
+++ routes/v1/api/candidate.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Candidates\AuthController;
 use App\Http\Controllers\API\Candidates\ProfileController;
@@ -8,25 +9,32 @@ use App\Http\Controllers\API\Candidates\Jobs\JobCategoryController;
 use App\Http\Controllers\API\Candidates\News\NewsController;
 use App\Http\Controllers\API\Candidates\News\NewsCategoryController;
 use App\Http\Controllers\API\Candidates\BannerController;
+use App\Http\Controllers\API\Candidates\CvController;
 
-Route::post('login',[AuthController::class,'login']);
-Route::post('register',[AuthController::class,'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('profile',[ProfileController::class,'get_profile']);
-    Route::post('profile',[ProfileController::class,'save_profile']);
-    Route::post('job-application',[JobApplicationController::class,'apply']);
-    Route::post('job-application/{id}',[JobApplicationController::class,'index']);
-    Route::post('job-application-list',[JobApplicationController::class,'list']);
-    Route::post('change-password',[ProfileController::class,'change_password']);
+    Route::get('profile', [ProfileController::class, 'get_profile']);
+    Route::post('profile', [ProfileController::class, 'save_profile']);
+    Route::post('job-application', [JobApplicationController::class, 'apply']);
+    Route::post('job-application/{id}', [JobApplicationController::class, 'index']);
+    Route::post('job-application-list', [JobApplicationController::class, 'list']);
+    Route::post('change-password', [ProfileController::class, 'change_password']);
+
+
+    // CV Upload
+    Route::post('/cv', [CvController::class, 'upload'])->name('candidate.cv.upload');
+    // Fetch CV
+    Route::get('/cv', [CvController::class, 'fetch'])->name('candidate.cv.fetch');
 });
 // Listing
-Route::get('job-list',[JobsListController::class,'list']);
-Route::get('job-categories',[JobCategoryController::class,'list']);
+Route::get('job-list', [JobsListController::class, 'list']);
+Route::get('job-categories', [JobCategoryController::class, 'list']);
 
 
-Route::get('news-categories',[NewsCategoryController::class,'list']);
-Route::get('news',[NewsController::class,'list']);
-Route::get('news/{id}',[NewsController::class,'index']);
+Route::get('news-categories', [NewsCategoryController::class, 'list']);
+Route::get('news', [NewsController::class, 'list']);
+Route::get('news/{id}', [NewsController::class, 'index']);
 
-Route::get('banners',[BannerController::class,'list']);
+Route::get('banners', [BannerController::class, 'list']);
