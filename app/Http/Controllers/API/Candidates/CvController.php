8eba2a7de27acffb6c9f4cb2306c068822403f79
prolaxu <adhikarilaxu0001@gmail.com>
2022-01-25 10:10:09 +0545

    BugFix - Upload Cv.

--- app/Http/Controllers/API/Candidates/CvController.php
+++ app/Http/Controllers/API/Candidates/CvController.php
@@ -31,21 +31,46 @@ class CvController extends Controller
     public function fetch()
     {
         $employ = Employe::where('user_id', auth()->user()->id)->first();
-        $cv = EmployCv::where('employ_id', $employ->id)->first();
-        return  $this->sendResponse($this->process($cv), "CV fetched successfully.");
+        try {
+            $cv_record = EmployCv::where('employ_id', $employ->id)->first();
+            return  $this->sendResponse($this->process($cv_record), "CV fetched successfully.");
+        } catch (\Throwable $th) {
+            $errors = ["No CV found."];
+            return  $this->sendError($errors);
+        }
     }
     private function process($cv)
     {
         return [
             "id" => $cv->id,
             "title" => $cv->title,
-            "cv_file" => env('APP_URL') . '/cv/' . $cv->cv_file,
+            "cv_file" => env('APP_URL') . $cv->cv_file,
         ];
     }
     public function store($file)
     {
         $imageName = time() . '.pdf';
         $file->move(public_path('/uploads/cv/'), $imageName);
-        return '/uploads/cv/' . $imageName;
+        return 'uploads/cv/' . $imageName;
+    }
+    public function edit(Request $request)
+    {
+        $employ = Employe::where('user_id', auth()->user()->id)->first();
+        $cv = EmployCv::where('id', $request->id)->where('employ_id', $employ->id)->first();
+        $cv->update(
+            [
+                'title' => $request->title ?? '',
+                'cv_file' => $this->store($request->file('cv_file')),
+            ]
+        );
+        return  $this->sendResponse($this->process($cv), "CV fetched successfully.");
+    }
+    // Delete CV file
+    public function delete($id)
+    {
+        $employ = Employe::where('user_id', auth()->user()->id)->first();
+        $cv = EmployCv::where('id', $id)->where('employ_id', $employ->id)->first();
+        $cv->delete();
+        return  $this->sendResponse([], "CV deleted successfully.");
     }
 }
