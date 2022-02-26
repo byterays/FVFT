8a2ab7341d823521433084f5bd4952f9ad711a0f
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-02-26 16:57:14 +0545

    job crud done

--- app/Http/Controllers/Company/JobController.php
+++ app/Http/Controllers/Company/JobController.php
@@ -16,9 +16,9 @@ class JobController extends Controller
 
     public function __construct()
     {
-        $this->experiencelevels = \DB::table('experiencelevels')->get();
-        $this->educationlevels = \DB::table('educationlevels')->get();
-        $this->job_shifts = \DB::table('job_shifts')->get();
+        $this->experiencelevels = DB::table('experiencelevels')->get();
+        $this->educationlevels = DB::table('educationlevels')->get();
+        $this->job_shifts = DB::table('job_shifts')->get();
     }
 
     public function addNewJob()
@@ -37,7 +37,71 @@ class JobController extends Controller
     public function saveNewJob(Request $request)
     {
         // dd($request->all());
-        $validator = Validator::make($request->all(), [
+        $validator = $this->__validation($request->all());
+
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()]);
+        }
+
+        if($validator->passes()){
+            try{
+                DB::beginTransaction();
+                $job = new Job();
+                $this->__saveOrUpdateJob($job, $request, '');
+                $job->status = 'Not Approved';
+                $job->save();
+                DB::commit();
+                return response()->json(['msg' => 'New job added', 'redirectRoute' => route($this->redirectTo)]);
+            } catch(\Exception $e){
+                DB::rollBack();
+                return response()->json(['db_error' => $e->getMessage()]);
+            }
+        }
+    }
+
+
+    public function edit($id)
+    {
+        $job = Job::findOrFail($id);
+        $fields = [
+            "job" => $job,
+            "experience_levels" => $this->experiencelevels,
+            "job_shifts" => $this->job_shifts,
+            "education_levels" => $this->educationlevels,
+        ];
+        return $this->company_view('company.job.editjob', $fields);
+    }
+
+
+    public function updateJob(Request $request, $id)
+    {
+        $validator = $this->__validation($request->all());
+
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()]);
+        }
+        if($validator->passes()){
+            try{
+                DB::beginTransaction();
+                $job = Job::find($id);
+                $oldImage = $job->feature_image_url;
+                $oldStatus = $job->status;
+                $this->__saveOrUpdateJob($job, $request, $oldImage);
+                $job->status = $request->status != null ? $request->status : $oldStatus;
+                $job->save();
+                DB::commit();
+                return response()->json(['msg' => 'Job updated successfully', 'redirectRoute' =>route($this->redirectTo)]);
+            } catch(\Exception $e){
+                DB::rollBack();
+                return response()->json(['db_error' => $e->getMessage()]);
+            }
+        }
+    }
+
+
+    private function __validation(array $data)
+    {
+        return Validator::make($data, [
             'job_title' => ['required'],
             'company_id' => ['required'],
             'featured_image' => ['nullable', 'image', 'mimes:jpeg,png,jpg', 'max:4096'],
@@ -53,49 +117,37 @@ class JobController extends Controller
             'education_level.required' => 'Education level is required',
             'experience_level.required' => 'Experience Level is required',
         ]);
+    }
 
-        if($validator->fails()){
-            return response()->json(['errors' => $validator->errors()]);
+    private function __saveOrUpdateJob($job, $request, $oldImage='')
+    {
+        $job->company_id = $request->company_id;
+        $job->title = $request->job_title;
+        $job->description = $request->description;
+        if($request->has('featured_image')){
+            $image = $request->file('featured_image');
+            $imageName = time() . '_' . $image->getClientOriginalName();
+            $job->feature_image_url = $this->destination . $imageName;
+            $image->move(public_path($this->destination, 'public'), $imageName);
+        } else {
+            $job->feature_image_url = $oldImage;
         }
-
-        if($validator->passes()){
-            try{
-                DB::beginTransaction();
-                $job = new Job();
-                $job->company_id = $request->company_id;
-                $job->title = $request->job_title;
-                $job->description = $request->description;
-                if($request->has('featured_image')){
-                    $image = $request->file('featured_image');
-                    $imageName = time() . '_' . $image->getClientOriginalName();
-                    $job->feature_image_url = $this->destination . $imageName;
-                    $image->move(public_path($this->destination, 'public'), $imageName);
-                }
-                
-                $job->benefits = $request->benefits;
-                $job->salary_from = $request->salary_from;
-                $job->salary_to = $request->salary_to;
-                $job->hide_salary = $request->hide_salary != null ? 1 : 0;
-                $job->salary_currency = null;
-                $job->job_categories_id = $request->job_category;
-                $job->job_shift_id = $request->job_shift;
-                $job->num_of_positions = $request->no_of_position;
-                $job->expiry_date = $request->deadline;
-                $job->education_level_id = $request->education_level;
-                $job->job_experience_id = $request->experience_level;
-                $job->is_active = 0;
-                $job->is_featured = $request->is_featured != null ? 1 : 0;
-                $job->country_id = $request->country_id;
-                $job->state_id = $request->state_id;
-                $job->city_id = $request->city_id;
-                $job->status = 'Not Approved';
-                $job->save();
-                DB::commit();
-                return response()->json(['msg' => 'New job added', 'redirectRoute' => route($this->redirectTo)]);
-            } catch(\Exception $e){
-                DB::rollBack();
-                return response()->json(['db_error' => $e->getMessage()]);
-            }
-        }
+        
+        $job->benefits = $request->benefits;
+        $job->salary_from = $request->salary_from;
+        $job->salary_to = $request->salary_to;
+        $job->hide_salary = $request->hide_salary != null ? 1 : 0;
+        $job->salary_currency = null;
+        $job->job_categories_id = $request->job_category;
+        $job->job_shift_id = $request->job_shift;
+        $job->num_of_positions = $request->no_of_position;
+        $job->expiry_date = $request->deadline;
+        $job->education_level_id = $request->education_level;
+        $job->job_experience_id = $request->experience_level;
+        $job->is_active = 0;
+        $job->is_featured = $request->is_featured != null ? 1 : 0;
+        $job->country_id = $request->country_id;
+        $job->state_id = $request->state_id;
+        $job->city_id = $request->city_id;
     }
 }
--- routes/v1/web/company.php
+++ routes/v1/web/company.php
@@ -16,11 +16,13 @@ Route::middleware(['auth', 'is_company'])->group(function () {
     Route::get('/view-my-profile', [DashController::class, 'show'])->name('company.view_profile');
 
     Route::get('/jobs', [DashController::class, 'jobs'])->name('company.jobs');
-    Route::get('/edit/job/{id}', [DashController::class, 'edit'])->name('company.editjob');
+    // Route::get('/edit/job/{id}', [DashController::class, 'edit'])->name('company.editjob');
     Route::get('/applicants', [DashController::class, 'applicants'])->name('company.applicants');
     Route::get('/settings', [DashController::class, 'profile'])->name('company.settings');
 
 
     Route::get('add-new-job', [JobController::class, "addNewJob"])->name("company.addNewJob");
     Route::post('save-new-job', [JobController::class, "saveNewJob"])->name("company.saveNewJob");
+    Route::get('/edit/job/{id}', [JobController::class, 'edit'])->name('company.editjob');
+    Route::put('/udpate/job/{id}', [JobController::class, 'updateJob'])->name('company.updateJob');
 });
