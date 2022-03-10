92127349b807618d300f47b7bb95d70ef690e555
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-03-10 16:26:23 +0545

    Language Model modified

--- app/Models/Language.php
+++ app/Models/Language.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Language extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id', 'lang', 'native', 'iso_code', 'is_active', 'is_default', 'created_at', 'updated_at'];
 }
