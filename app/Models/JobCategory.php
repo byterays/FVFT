dff4dfc0a3957353ccc38f5f380b90c50bbe6f42
Muniraj Rajbanshi <munirajrajbanshi@gmail.com>
2022-03-10 16:22:08 +0545

    JobCategory and migration modified

--- app/Models/JobCategory.php
+++ app/Models/JobCategory.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class JobCategory extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['id', 'functional_area', 'image_url', 'is_default', 'is_active', 'sort_order', 'lang', 'created_at', 'updated_at'];
 }
--- database/migrations/2021_12_24_081238_create_job_categories_table.php
+++ database/migrations/2021_12_24_081238_create_job_categories_table.php
@@ -16,6 +16,7 @@ class CreateJobCategoriesTable extends Migration
         Schema::create('job_categories', function (Blueprint $table) {
             $table->bigIncrements('id')->unique('id');
             $table->string('functional_area', 200)->nullable();
+            $table->text('image_url')->nullable();
             $table->boolean('is_default')->nullable()->default(false);
             $table->boolean('is_active')->nullable();
             $table->integer('sort_order')->nullable()->default(99999);
