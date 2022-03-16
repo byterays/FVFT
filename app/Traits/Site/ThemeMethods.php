fd766fe5e0a93d671c2728d7b8c904c95bcbeeef
Bishal Khatri <bishal.khatri343@gmail.com>
2022-03-16 12:42:35 +0545

    fixes

--- app/Traits/Site/ThemeMethods.php
+++ app/Traits/Site/ThemeMethods.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 namespace App\Traits\Site;
 use Illuminate\Http\Response;
 use DB;
@@ -8,7 +8,7 @@ trait ThemeMethods{
         $theme="fvft";
         $primary_menu=DB::table('menu_items')->where(["menu_id"=>1,"parent_id"=>0])->get();
         // $countries=DB::table('countries')->where('is_active',1)->get();
-        $countries = DB::table('countries')->get();
+        $countries = DB::table('countries')->where('is_active', 1)->get();
         // $job_categories=DB::table('job_categories')->where('is_active',1)->get();
         $job_categories=DB::table('job_categories')->get();
         return view("themes.".$theme.".".$path,array_merge($obj,[
