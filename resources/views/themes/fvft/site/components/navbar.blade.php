927e5a1176b7869fd2bd95432b7093feda715783
Bishal Khatri <bishal.khatri343@gmail.com>
2022-04-08 12:51:35 +0545

    fixes

--- resources/views/themes/fvft/site/components/navbar.blade.php
+++ resources/views/themes/fvft/site/components/navbar.blade.php
@@ -1,56 +1,56 @@
 <nav class="horizontalMenu clearfix d-md-flex">
     <ul class="horizontalMenu-list">
         @foreach ($primary_menu as $item)
-        <li><a href="{{$item->link}}">{{$item->title}}</a></li>
+            <li><a href="{{$item->link}}">{{__($item->title)}}</a></li>
         @endforeach
         @auth
-        <li><a href="#">My Dashboard <span class="fa fa-caret-down m-0"></span></a>
-            <ul class="sub-menu">
-                @if(auth()->user()->user_type=='candidate')
-                <li>
-                    <a href="/candidate/">My Dashboard</a>
-                </li>
-                <li>
-                    <a href="/candidate/profile">Profile</a>
-                </li>
-                <li>
-                    <a href="/candidate/jobs">My Jobs</a>
-                </li>
-                <li>
-                    <a href="/candidate/settings">Settings</a>
-                </li>
-                @elseif(auth()->user()->user_type=='company')
-                <li>
-                    <a href="/company/">My Dashboard</a>
-                </li>
-                <li>
-                    <a href="/company/profile">Edit Profile</a>
-                </li>
-                <li>
-                    <a href="/company/jobs">My Jobs</a>
-                </li>
-                <li>
-                    <a href="/company/applicants">Applicants</a>
-                </li>
+            <li><a href="#">{{ __('My Dashboard') }} <span class="fa fa-caret-down m-0"></span></a>
+                <ul class="sub-menu">
+                    @if(auth()->user()->user_type=='candidate')
+                        <li>
+                            <a href="/candidate/">{{ __('My Dashboard') }}</a>
+                        </li>
+                        <li>
+                            <a href="/candidate/profile">{{ __('Profile') }}</a>
+                        </li>
+                        <li>
+                            <a href="/candidate/jobs">{{ __('My Jobs') }}</a>
+                        </li>
+                        <li>
+                            <a href="/candidate/settings">{{ __('Settings') }}</a>
+                        </li>
+                    @elseif(auth()->user()->user_type=='company')
+                        <li>
+                            <a href="/company/">{{ __('My Dashboard') }}</a>
+                        </li>
+                        <li>
+                            <a href="/company/profile">{{ __('Edit Profile') }}</a>
+                        </li>
+                        <li>
+                            <a href="/company/jobs">{{ __('My Jobs') }}</a>
+                        </li>
+                        <li>
+                            <a href="/company/applicants">{{ __('Applicants') }}</a>
+                        </li>
 
-                <li>
-                    <a href="/company/settings">Settings</a>
-                </li>
-                @elseif(auth()->user()->user_type=='admin')
-                <li>
-                    <a href="/admin/">My Dashboard</a>
-                </li>
-                @endif
-                <li>
-                    <form action="/logout" method="post">
-                      @csrf
-                      <button type="submit" class="btn btn-danger btn-sm text-white btn-block">
-                        <span class="icon1 " style="background: #ffffff00;color: #f3f6ff;"><i class="typcn typcn-power-outline fs-20"></i></span> Logout
-                    </button>
-                    </form>
-                </li>
-            </ul>
-        </li>
+                        <li>
+                            <a href="/company/settings">{{ __('Settings') }}</a>
+                        </li>
+                    @elseif(auth()->user()->user_type=='admin')
+                        <li>
+                            <a href="/admin/">{{ __('My Dashboard') }}</a>
+                        </li>
+                    @endif
+                    <li>
+                        <form action="/logout" method="post">
+                            @csrf
+                            <button type="submit" class="btn btn-danger btn-sm text-white btn-block">
+                                <span class="icon1 " style="background: #ffffff00;color: #f3f6ff;"><i class="typcn typcn-power-outline fs-20"></i></span> {{ __('Logout') }}
+                            </button>
+                        </form>
+                    </li>
+                </ul>
+            </li>
         @endauth
 
     </ul>
