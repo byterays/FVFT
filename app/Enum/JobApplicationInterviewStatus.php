6155b11e16506db557e8c71535e00dfff5664399
Bishal Khatri <bishal.khatri343@gmail.com>
2022-04-25 10:12:31 +0545

    application list updated

--- app/Enum/JobApplicationInterviewStatus.php
+++ app/Enum/JobApplicationInterviewStatus.php
@@ -3,5 +3,8 @@ namespace App\Enum;
 
 interface JobApplicationInterviewStatus
 {
+    const STARTED = 'started';
     const NOT_STARTED = 'notstarted';
+    const FAIL = 'fail';
+    const PASS = 'pass';
 }
