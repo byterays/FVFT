845a0e349184b61acc8ee15b14f64abba3a1d333
Bishal Khatri <bishal.khatri343@gmail.com>
2022-06-06 14:24:59 +0545

    enhancement and fixes in candidate profile>settings

--- resources/views/themes/fvft/candidates/setting/job_setting/get_job_alert.blade.php
+++ resources/views/themes/fvft/candidates/setting/job_setting/get_job_alert.blade.php
@@ -24,15 +24,7 @@
                 </div>
                 <div class="col-xl-9 col-lg-12 col-md-12">
                     <div class="row">
-                        <div class="card mb-2">
-                            <div class="card-body">
-                                <h3 class="font-weight-bold">{{ strtoupper(__('Settings')) }}</h3>
-                                <div id="basicwizard" class="border pt-0">
-                                    @include('partial/candidates/setting_tabs')
-                                </div>
-                            </div>
-                        </div>
-                        @include('partial/candidates/jobsetting/pilltab')
+                        @include('partial/candidates/setting_tabs', ['title' => 'Settings - Job Alert Setting'])
                     </div>
                     <div class="row">
                         <div class="card mb-0">
